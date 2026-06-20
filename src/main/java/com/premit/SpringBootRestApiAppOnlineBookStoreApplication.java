24007cb19c3e33691840513bf58f4103f34434ec
mutha <mutha@PREMSAI-LAPPY.hgu_lan>
2026-06-20 16:44:11 +0530

    premsai7744 : Initialize Project setup for spring-boot-rest-app-online-book-store

---
+++ src/main/java/com/premit/SpringBootRestApiAppOnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.premit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiAppOnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiAppOnlineBookStoreApplication.class, args);
+	}
+
+}
